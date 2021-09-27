d1991590d08448d92d11667b3a1b1c07c2a6edba
Dong Nguyen <dongnguyenvie@gmail.com>
2021-09-28 01:37:59 +0700

    no message

--- src/modules/auth/dto/signup.dto.ts
+++ src/modules/auth/dto/signup.dto.ts
@@ -1,9 +1,11 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export default class SignupDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 }
