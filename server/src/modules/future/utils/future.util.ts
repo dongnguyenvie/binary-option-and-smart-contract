beb8ae6963d2776f65ab524f280df226c16eda4c
Nolan <dongnguyenvie@gmail.com>
2023-04-18 03:02:04 +0700

    fixed time betting

--- server/src/modules/future/utils/future.util.ts
+++ server/src/modules/future/utils/future.util.ts
@@ -1,7 +1,7 @@
 import dayjs from 'src/modules/shared/helpers/dayjs';
 
 export function isCanCalculateBetting(time: number) {
-  const nextCandleTime = dayjs().startOf('minute').unix() * 1000;
+  // const nextCandleTime = dayjs().startOf('minute').unix() * 1000;
 
   return time && (time / 1000 / 60) % 2 === 1;
 }
